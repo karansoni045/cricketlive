861b90b4019eb133e2b1994d95c35d5c1254f350
Karan Soni <140481377+karansoni045@users.noreply.github.com>
2025-05-12 15:05:13 +0100

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,4 @@
+// Optional: Any JS functionality can be added for controlling playback
+document.getElementById('live-stream').addEventListener('play', function() {
+  console.log('Stream started!');
+});
