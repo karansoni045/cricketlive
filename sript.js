1cd4e243935eaf4503f3c4f9af6d04f5ec6ad503
Karan Soni <140481377+karansoni045@users.noreply.github.com>
2026-01-11 14:08:33 +0530

    Delete sript.js

--- sript.js
+++
@@ -1,4 +0,0 @@
-// Optional: Any JS functionality can be added for controlling playback
-document.getElementById('live-stream').addEventListener('play', function() {
-  console.log('Stream started!');
-});
